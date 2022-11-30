437547b15f4990e5dd2927d39631856d9924db42
sunitha <sunithapraveenkumar@gmail.com>
2022-11-30 16:24:05 +0530

    1. completed splash

---
+++ app/src/main/java/com/improve10x/crud/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.improve10x.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        Intent intent = new Intent(this,DashBoardActivity.class);
+        startActivity(intent);
+        handler.postDelayed((() -> {
+        }),5000);
+
+    }
+}
